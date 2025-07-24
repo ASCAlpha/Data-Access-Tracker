9c954acbb6994e764f083234049370de229117d6
ASCAlpha <abdu.st.clair@cynatglobal.com>
2025-07-24 16:02:21 -0400

    Create keyboard_navigation.js

---
+++ keyboard_navigation.js
@@ -0,0 +1,23 @@
+The keyboard_navigation.js file, as described in the Canvas document, is a JavaScript script designed to enhance keyboard accessibility for the Data Access Tracker's web pages.
+
+It provides the following key features for keyboard navigation:
+
+Sidebar Control:
+
+You can toggle the sidebar open or closed using the keyboard shortcut Alt + S.
+
+Once the sidebar is open, you can navigate between its links using the ArrowUp and ArrowDown keys.
+
+Pressing the Escape key will close the sidebar.
+
+User Dropdown Control:
+
+The user dropdown menu can be toggled open or closed using Alt + U.
+
+When the user avatar (which acts as the toggle) is focused, you can also open the dropdown by pressing Enter or Space.
+
+Within the open dropdown, ArrowUp and ArrowDown keys allow you to navigate through the menu items.
+
+The Escape key will close the user dropdown.
+
+This script aims to improve the overall usability of the application for users who prefer or require keyboard navigation.
